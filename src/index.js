c84cd81df4bfac39fd7be7ea6f80691fc99f5c31
Basu <basudevkumar.india2019@gmail.com>
2025-06-01 12:08:07 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,75 @@
+// require('dotenv').config({path:"./env"});                  // ok approach
+
+//  here improve approach
+
+import dotenv from "dotenv";
+
+
+import connectDB from './db/indexMongo.js';
+
+
+dotenv.config({
+    path:"./env"
+})
+
+
+connectDB()
+
+
+
+
+
+
+// 1st appraoch to connect database 
+// for optimal approach , we use different file folder to connect them
+
+
+/*
+
+import express from 'express';
+const app=express();
+
+// 1st way
+// function connectDB(){}
+
+
+
+
+// connectDB()
+
+
+// better way
+
+(async ()=>{
+
+    try {
+
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+
+        // app.on("Error",()=>{
+        //     console.log("ERror",error);
+        //     throw error;
+        // })
+
+        app.on("Error",(error)=>{
+            console.log("ERR",Error);
+            throw error;
+            
+        });
+
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on Port ${process.env.PORT}`);
+        });
+    }
+    
+    catch (error) {
+        console.error("ERROR",error);
+        throw error;
+    }
+
+})()
+
+
+
+*/

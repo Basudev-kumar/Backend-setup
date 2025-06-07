6007c635282d15e861b859243f985d0028e46090
Basu <basudevkumar.india2019@gmail.com>
2025-06-07 11:43:05 +0530

    Add middleware

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// const asyncHandler=()=>{};
+
+
+
+export {asyncHandler};
+
+
+
+// const asyncHandler=()=>{};
+
+// const asyncHandler=(fn)=>{};
+
+// const asyncHandler=(fn)=>async ()=>{};
+
+
+
+const asyncHandler=(fn)=>async ()=>{
+
+    try {
+
+
+        
+    } catch (error) {
+        
+        console.log("Middleware Error",error);
+        
+    }
+};

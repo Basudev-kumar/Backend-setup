8056a06bd18e6bf7ecc2086467752c919575a4d4
Basu <basudevkumar.india2019@gmail.com>
2025-06-08 12:48:21 +0530

    userModel

---
+++ src/models/user.model.js
@@ -0,0 +1,99 @@
+import mongoose , {Schema} from 'mongoose';
+
+const userSchema = new Schema (
+
+    {
+
+        
+        id:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        
+        watchHistory:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:'Video'
+            }
+        ],
+
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        
+        },
+
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+        },
+
+        avatar:{
+            type:String,                   // cloudary url
+            required:true,
+        
+        
+        },
+
+        coverImage:{
+            type:String,                  // cloudary url
+           
+            
+        },
+    
+        
+        password:{
+            type:String,
+            required:[true,"Password is required"],
+        
+        },
+
+        
+        refreshToken:{
+            type:String,
+    
+        },
+
+        
+        createAt:{
+            type:Date,
+            
+        },
+
+        
+        updateAt:{
+            type:String,
+            
+        },
+
+
+
+    
+    },
+    {
+        timestamps:true
+    }
+
+);
+
+
+
+export const User=mongoose.model("User",userSchema);

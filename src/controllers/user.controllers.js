fc2674c4675d90557961ba9e597050ac2f1a5c6d
Basu <basudevkumar.india2019@gmail.com>
2025-06-17 01:17:13 +0530

    all set configure

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,33 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
 
 const registerUser=asyncHandler(async (req,res) =>{
-    res.status(200).json({
-        message:"Ok Done!"
-    })
+    
+    // testing
+    // res.status(200).json({
+    //     message:"Ok Done!"
+    // })
+
+
+
+
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exits : username , email
+    // check for images , check for avatar
+    // upload them to cloudinary , avatar
+    // create user object , create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+    const {fullName , username , email , password} = req.body;
+    console.log("email : ",email)
+
+    if (fullName==="") {
+        throw new ApiError(400,"Fullname is required");
+    }
+
 })
 
 
